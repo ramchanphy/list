2b3e2125c049be82a960951dbebea62a6e45f744
Ramchanphy <you@example.com>
2022-01-06 20:39:52 +0530

    y first commit

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+name=['n','i','t','i','i']
+i=-1
+a=[]
+while i>=(-(len(name))):
+    a.append(name[i])
+    i-=1
+if a==name:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
+    
---
+++ reverse2.py
@@ -0,0 +1,6 @@
+places=["delhi","gujrat","rajasthan","punjab","kerela"]
+i=1 
+while i<=(len(places)):
+    print(places[-i])
+    i+=1
+    
